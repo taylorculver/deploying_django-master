4197ccbb1407fdc35c1da9e2032820b17e1dea29
TCulver84 <taylor.culver@gmail.com>
2018-07-09 17:28:22 -0400

    V2

---
+++ djangoal/djangoal/deploy.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for djangoal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise.django import DjangoWhiteNoise
+
+path = "/home/taylorculver/deploying_django-master/djangogoal"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoal.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
